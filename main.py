97ba9b880971b381444b8d2e4b6f54342bb189e6
Billy Arante <arantebillywilson@gmail.com>
2023-07-20 14:25:19 +0800

    feat(main.py): print a report

--- main.py
+++ main.py
@@ -15,7 +15,21 @@ def count_words(contents):
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(count_letters(file_contents))
+
+    filename = "frankenstein.txt"
+    print(f"**** Report of books/{filename} ****\n")
+
+    print(f"{count_words(file_contents)} words found in the document\n")
+
+    letters = count_letters(file_contents)
+    letters_list = list(letters.items())
+    letters_list.sort(key=lambda x: x[1], reverse=True)
+    for item in letters_list:
+        if item[0].isalpha():
+            print(f"The '{item[0]}' character was found {item[1]} times")
+    print()
+
+    print("**** End ****")
 
 
 if __name__ == "__main__":
