ffebc3007cdc88eeeb3e44ac20b17a7fea8ae39f
Billy Arante <arantebillywilson@gmail.com>
2026-01-07 22:39:02 +0800

    refactor: ♻️ use sys module

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from stats import character_items, get_book_text, count_words, count_characters
+import sys
 
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
     print("============ BOOKBOT ============")
-    book_file_path = "books/frankenstein.txt"
+    book_file_path = sys.argv[1]
     print(f"Analyzing book found at {book_file_path}")
 
     print("----------- Word Count ----------")
@@ -14,7 +19,7 @@ def main():
     characters_dict = count_characters(book_text)
     items = character_items(characters_dict)
     for i in items:
-        print(f"{i["char"]}: {i["num"]}")
+        print(f"{i['char']}: {i['num']}")
 
 
 if __name__ == "__main__":
