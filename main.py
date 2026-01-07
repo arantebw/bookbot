e53430379e4ccc6e1d54bdc905d0bb0b2ae2a398
Billy Arante <arantebillywilson@gmail.com>
2026-01-07 22:05:01 +0800

    feat: ✨ add count_characters function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from stats import get_book_text, count_words
+from stats import get_book_text, count_words, count_characters
 
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     print(f"Found {count_words(book_text)} total words")
+    print(count_characters(book_text))
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
+def count_characters(book_text: str):
+    letter_count = {}
+    for letter in book_text.lower():
+        if letter in letter_count:
+            letter_count[letter] += 1
+        else:
+            letter_count[letter] = 1
+    return letter_count
+
+
 def count_words(book_text: str):
     return len(book_text.split())
 
