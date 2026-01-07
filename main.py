64846f42b1b50e7afba1b115d6e1fe7604ca22ee
Billy Arante <arantebillywilson@gmail.com>
2026-01-07 21:33:43 +0800

    feat: ✨ add get_book_text and main functions

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(file_path):
+    file_contents = ""
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+
+if __name__ == "__main__":
+    main()
