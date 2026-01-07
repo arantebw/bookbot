aaddf4f14c890125f8629d512b32eabe38cc94b1
Billy Arante <arantebillywilson@gmail.com>
2026-01-07 22:30:17 +0800

    feat: ✨ add pretty report

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from stats import get_book_text, count_words, count_characters
+from stats import character_items, get_book_text, count_words, count_characters
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    book_file_path = "books/frankenstein.txt"
+    print(f"Analyzing book found at {book_file_path}")
+
+    print("----------- Word Count ----------")
+    book_text = get_book_text(book_file_path)
     print(f"Found {count_words(book_text)} total words")
-    print(count_characters(book_text))
+
+    print("--------- Character Count -------")
+    characters_dict = count_characters(book_text)
+    items = character_items(characters_dict)
+    for i in items:
+        print(f"{i["char"]}: {i["num"]}")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
+def sort_on(item: dict):
+    return item["num"]
+
+
+def character_items(characters_dict: dict):
+    items = [item for item in characters_dict.items()]
+    character_items: list[dict] = []
+    for key, value in items:
+        char_item = {"char": key, "num": value}
+        character_items.append(char_item)
+    character_items.sort(key=sort_on, reverse=True)
+    return character_items
+
+
 def count_characters(book_text: str):
     letter_count = {}
     for letter in book_text.lower():
