b5a57d40f09fcfd7b784f91da1038f16fd63e9cd
Billy Arante <arantebillywilson@gmail.com>
2026-01-07 21:41:37 +0800

    feat: ✨ add count_words function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def get_book_text(file_path):
+def count_words(book_text: str):
+    return len(book_text.split())
+
+
+def get_book_text(file_path: str):
     file_contents = ""
     with open(file_path) as f:
         file_contents = f.read()
@@ -7,7 +11,7 @@ def get_book_text(file_path):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    print(f"Found {count_words(book_text)} total words")
 
 
 if __name__ == "__main__":
